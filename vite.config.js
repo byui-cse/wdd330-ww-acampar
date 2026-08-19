0a46c91e36ff4191817962c61b33abb8f5383cde
davi-fts <fre24036@byui.edu>
2026-08-19 13:40:46 -0600

    Rename product page paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,20 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
+        carrinho: resolve(__dirname, "src/carrinho/index.html"),
+        finalizar_compra: resolve(__dirname, "src/finalizar_compra/index.html"),
+        produto1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
+          "src/paginas_produto/cedar-ridge-rimrock-2.html",
         ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
+        produto2: resolve(__dirname, "src/paginas_produto/marmot-ajax-3.html"),
+        produto3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html",
+          "src/paginas_produto/northface-alpine-3.html",
         ),
-        product4: resolve(
+        produto4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html",
+          "src/paginas_produto/northface-talus-4.html",
         ),
       },
     },
