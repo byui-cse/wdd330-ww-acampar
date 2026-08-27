6eff70b9f7d09c3710b6141e7438d706781b3d6b
Davi Freitas <davi.freitasgeral@gmail.com>
2026-08-27 15:18:13 -0300

    fix(pt-br): corrige bugs de funcionalidade no carrinho e produto
    - corrige typo obterArmazeNomentoLocal -> obterArmazenamentoLocal (import quebrado
      fazia a pagina do carrinho quebrar com TypeError)
    - corrige typo de classe cartao-carrinho__Imagemm -> cartao-carrinho__imagem
      (nao batia com a classe definida no CSS, quebrando o layout do item do carrinho)
    - corrige chamada a metodo inexistente encontrarProdutoPorId -> buscarProdutoPorId
      em produto.js (quebrava o botao Adicionar ao Carrinho)
    - padroniza simbolo de moeda para R$ no item renderizado do carrinho

--- src/js/carrinho.js
+++ src/js/carrinho.js
@@ -1,15 +1,15 @@
 // carrinho.js
-import { obterArmazeNomentoLocal } from "./uteis.mjs";
+import { obterArmazenamentoLocal } from "./uteis.mjs";
 
 function renderizarConteudoCarrinho() {
-  const itensCarrinho = obterArmazeNomentoLocal("so-carrinho");
+  const itensCarrinho = obterArmazenamentoLocal("so-carrinho");
   const itensHtml = itensCarrinho.map((item) => modeloItemCarrinho(item));
   document.querySelector(".lista-produtos").innerHTML = itensHtml.join("");
 }
 
 function modeloItemCarrinho(item) {
   const novoItem = `<li class="cartao-carrinho divisor">
-  <a href="#" class="cartao-carrinho__Imagemm">
+  <a href="#" class="cartao-carrinho__imagem">
     <img
       src="${item.Imagem}"
       alt="${item.Nome}"
@@ -20,7 +20,7 @@ function modeloItemCarrinho(item) {
   </a>
   <p class="cartao-carrinho__cor">${item.Cores[0].NomeCor}</p>
   <p class="cartao-carrinho__quantidade">qtd: 1</p>
-  <p class="cartao-carrinho__preco">$${item.PrecoFinal}</p>
+  <p class="cartao-carrinho__preco">R$${item.PrecoFinal}</p>
 </li>`;
 
   return novoItem;
